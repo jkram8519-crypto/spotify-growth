2382954df91e5bbe860ad94865f6ced16f002848
jkram <j.k.ram8519@gmail.com>
2026-06-26 00:22:44 +0200

    Ajout Google Analytics pour suivi du tunnel de conversion

--- app/layout.tsx
+++ app/layout.tsx
@@ -56,6 +56,7 @@ export default function RootLayout({
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
           gtag('config', 'AW-18217088729');
+          gtag('config', 'G-QJ3R4P3EWZ');
         `}
       </Script>
       <body>
